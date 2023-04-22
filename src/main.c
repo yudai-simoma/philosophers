30fe156bad2ec7df4cb73d4a5506d72d776658d2
yudai-shimoma <cdcdasdll@icloud.com>
2023-04-22 15:09:31 +0900

    shimoma : 2023/04/22 	add : Makefile、ヘッダーファイル修正、1人1人の哲学者の作成

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: yshimoma <yshimoma@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 19:07:41 by yshimoma          #+#    #+#             */
-/*   Updated: 2023/04/21 19:32:24 by yshimoma         ###   ########.fr       */
+/*   Updated: 2023/04/22 13:12:05 by yshimoma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,11 +46,46 @@ bool	ft_set_pram(int ac, char **av, t_philosophers *philosophers)
 	philosophers->die_time = atoi(av[2]);
 	philosophers->eat_time = atoi(av[3]);
 	philosophers->sleep_time = atoi(av[4]);
-	philosophers->remaining_forks = philosophers->num_people;
+	// philosophers->remaining_forks = philosophers->num_people;
 	if (ac == 6)
 		philosophers->philosopher_eating_count = atoi(av[5]);
+	return (false);
 }
 
+bool	ft_set_philosopher(t_philosophers *philosophers)
+{
+	size_t	i_;
+
+	philosophers->philosopher = (t_philosopher *)malloc(
+			sizeof(t_philosopher) * philosophers->num_people);
+	if (philosophers->philosopher == NULL)
+		return (true);
+	i_ = 0;
+	while (i_ < philosophers->num_people)
+	{
+		philosophers->philosopher[i_].number = (int)i_;
+		philosophers->philosopher[i_].status = IDLE;
+		if (pthread_mutex_init(
+				&philosophers->philosopher[i_].right_fork, NULL) != 0)
+		{
+			free(philosophers->philosopher);
+			return (true);
+		}
+		if (pthread_mutex_init(
+				&philosophers->philosopher[i_].left_fork, NULL) != 0)
+		{
+			free(philosophers->philosopher);
+			return (true);
+		}
+		i_++;
+	}
+	return (false);
+}
+
+/*
+ * TODO:
+ * 	pthread_mutex_destroyの使用
+ */
 int	main(void)
 {
 	char *argv[6] = {"./philosophers", "3", "200", "50", "50", "5"};
@@ -58,7 +93,9 @@ int	main(void)
 	t_philosophers	philosophers;
 
 	if (ft_is_error(argc, argv)
-		|| ft_set_pram(argc, argv, &philosophers))
+		|| ft_set_pram(argc, argv, &philosophers)
+		|| ft_set_philosopher(&philosophers)
+		|| ft_create_philo(&philosophers))
 	{
 		printf ("Error\n");
 		return (0);
